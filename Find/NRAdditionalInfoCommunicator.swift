2bb0262a005d4161a4e0dc850bb0396dd86b2a91
Jonathon Toon <contact@jonathontoon.com>
2015-04-26 14:02:17 -0700

    add

---
+++ Find/NRAdditionalInfoCommunicator.swift
@@ -0,0 +1,33 @@
+//
+//  NRAdditionalInfoCommunicator.swift
+//  Find
+//
+//  Created by Jonathon Toon on 3/1/15.
+//  Copyright (c) 2015 Jonathon Toon. All rights reserved.
+//
+
+import Foundation
+
+class NRAdditionalInfoCommunicator: NSObject {
+    
+    var delegate: NRAdditionalInfoCommunicatorDelegate!
+    
+    func getAdditionalInfoForDomain(query: String!) {
+        
+        let urlAsString: String = String(format: "https://www.kimonolabs.com/api/ondemand/2mp3abs4?apikey=e64b763681f140bec8391a4e8547d9dd&kimpath1=%@&q=%@", query.stringByReplacingOccurrencesOfString(" ", withString: ""))
+        
+        let url: NSURL = NSURL(string: urlAsString)!
+        
+        NSURLConnection.sendAsynchronousRequest(NSURLRequest(URL: url), queue: NSOperationQueue(), completionHandler: {
+            (response: NSURLResponse!, data: NSData!, error: NSError?)  -> Void in
+            
+            if error != nil {
+                self.delegate.additionalInfoJSONFailedWithError(error!)
+            } else {
+                self.delegate.receivedAdditionalInfoJSON(data)
+            }
+            
+        })
+        
+    }
+}
